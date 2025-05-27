1b7b1a5ce6114f4339aa49754ab84ba56c2567c2
유지수 <jsyu333@gmail.com>
2025-05-28 02:18:57 +0900

    chore: update page content

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,5 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './font.css'
 import App from './App.tsx'
-import 'locomotive-scroll/dist/locomotive-scroll.css'
 
 createRoot(document.getElementById('root')!).render(<App />)
