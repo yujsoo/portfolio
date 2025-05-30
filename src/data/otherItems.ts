10c940086fa652ab601e0d4bd6e15b9ee7bd4822
유지수 <jsyu333@gmail.com>
2025-05-30 17:06:16 +0900

    chore: fix typo

--- src/data/otherItems.ts
+++ src/data/otherItems.ts
@@ -2,14 +2,12 @@ export const otherItems = [
   {
     id: 100,
     title: '웹접근성 명도대비 검사 도구',
-    subtitle: '작은 차이가 사용자 경험을 만든다.',
     date: 'Apr.22 - Jun.22',
     link: '/project/contrast',
   },
   {
     id: 101,
     title: '색상 코드 변환 도구',
-    subtitle: '색을 바꾸는 가장 빠른 방법',
     date: 'Apr.22 - Jun.22',
     link: '/project/colorcode',
   },
--- src/pages/ProjectCode.tsx
+++ src/pages/ProjectCode.tsx
@@ -8,12 +8,13 @@ import { IoLogoJavascript } from 'react-icons/io5'
 
 function ProjectCode() {
   return (
-    <ProjectReviewLayout title="컬러코드 변환기" date="Apr.22 - Jun.22">
+    <ProjectReviewLayout title="색상 코드 변환 도구" date="Apr.22 - Jun.22">
       <ul>
         <li className="mb-14">
           <ProjectSubTitle title="Overview." />
           <p className="leading-relaxed">
-            컬러코드 변환 도구를 기획하고 개발하여, 실무자들이 HEX, RGB, HSL 간의 색상 포맷을 실시간으로 변환할 수 있도록 구현했습니다.
+            색상 코드 변환 도구를 기획하고 개발하여, 실무자들이 HEX, RGB, HSL
+            간의 색상 포맷을 실시간으로 변환할 수 있도록 구현했습니다.
           </p>
           <div className="mt-10">
             <Link
@@ -41,10 +42,10 @@ function ProjectCode() {
           <ProjectSubTitle title="Main Tasks." />
           <div className="flex flex-col gap-2">
             <p className="relative pl-4 before:absolute before:left-0 before:top-0  before:content-['-']">
-                HEX, RGB, HSL 간 변환이 가능한 컬러코드 실시간 변환 도구 개발
+              HEX, RGB, HSL 간 변환이 가능한 컬러코드 실시간 변환 도구 개발
             </p>
             <p className="relative pl-4 before:absolute before:left-0 before:top-0  before:content-['-']">
-                실무자 편의를 고려한 직관적인 UI 및 즉시 결과 출력 처리 구현
+              실무자 편의를 고려한 직관적인 UI 및 즉시 결과 출력 처리 구현
             </p>
           </div>
         </li>
@@ -52,13 +53,13 @@ function ProjectCode() {
           <ProjectSubTitle title="Tech Stack." />
           <div className="flex gap-2">
             <p className="flex justify-center items-center w-12 h-12 border border-[#ddd] rounded-md">
-                <FaHtml5 className="text-[#E34F26] text-2xl" />
+              <FaHtml5 className="text-[#E34F26] text-2xl" />
             </p>
             <p className="flex justify-center items-center w-12 h-12 border border-[#ddd] rounded-md">
-                <FaCss3Alt className="text-[#1572B6] text-2xl" />
+              <FaCss3Alt className="text-[#1572B6] text-2xl" />
             </p>
             <p className="flex justify-center items-center w-12 h-12 border border-[#ddd] rounded-md">
-                <IoLogoJavascript className="text-[#F7DF1E] text-2xl" />
+              <IoLogoJavascript className="text-[#F7DF1E] text-2xl" />
             </p>
           </div>
         </li>
